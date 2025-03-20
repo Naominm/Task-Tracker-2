40611417f9a945941c4c0a1a75070c6fdc1ad4ee
Naominm <naomimbugua536@gmail.com>
2025-03-20 13:15:15 +0300

    Build handle mark as complete and mark as incomplete

--- src/Todo/TodoItem.jsx
+++ src/Todo/TodoItem.jsx
@@ -1,14 +1,26 @@
-
+import useTaskStore from "../store/tasksStore";
 function TodoItem({title, description,complete,id}) {
+  const markAsCompleted=  useTaskStore(function(state){
+        return state.markComplete
+    })
+   const markAsIncomplete= useTaskStore(function(state){
+        return state.markIncomplete
+    })
+    function handleMarkIncomplete(){
+        markAsIncomplete(id)
+    }
+    function handleMarkComplete(){
+        markAsCompleted(id)
+    }
     return ( 
         <div className="todo-item-container">
             <div className="text-container">
-            <h1 className="title">{title}</h1>
-            <p className="description">{description}</p>
+            <h1 className={complete? `task-title complete`:`task-title`}>{title}</h1>
+            <p className={complete? `complete`:``}>{description}</p>
             </div>
             
             <div className="todo-input-controls">
-                <button>Mark as complete</button>
+                <button  onClick={complete? handleMarkIncomplete:handleMarkComplete}>{complete? "Mark as incomplete":"Mark as complete"} </button>
                 <button>Delete </button>
             </div>
         </div>
--- src/store/tasksStore.js
+++ src/store/tasksStore.js
@@ -8,6 +8,28 @@ addTask:function(newTask){
 set(function(previousState){
     return{tasks:[newTask,...previousState.tasks]}
 })
+},
+markComplete:function(taskId){
+    set(function(previousState){
+const updatedTask=previousState.tasks.map(function(task){
+    if(task.id===taskId){
+        task.complete=true
+    }
+    return task;
+})
+return{tasks:updatedTask}
+    })
+},
+markIncomplete:function(taskId){
+set(function(previousState){
+   const updatedTask= previousState.tasks.map(function(task){
+        if(task.id===taskId){
+            task.complete=false
+        }
+        return task
+    })
+    return{tasks:updatedTask}
+})
 }
 }
 }
