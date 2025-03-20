5b1f530530afba3d0d86702649a62137e6f5a3d3
Naominm <naomimbugua536@gmail.com>
2025-03-20 11:12:16 +0300

    Initial project reset

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Hero from "./Todo/Hero";
+
+function App() {
+  return ( 
+    <div>
+     <Hero/>
+    </div>
+   );
+}
+
+export default App;
---
+++ src/Todo/Hero.jsx
@@ -0,0 +1,9 @@
+
+function Hero() {
+    return ( 
+    <section className="hero-section-container">
+<h1>this is where the hero will be located</h1>
+    </section> );
+}
+
+export default Hero;
